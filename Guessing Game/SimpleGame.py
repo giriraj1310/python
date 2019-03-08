a11087ad85fcff2ae7fc55225aba5df134ac9971
Xcode User <xcode@Fasts-MacBook-Pro-2.local>
2019-03-07 20:00:50 -0500

    Keep asking the player until he wins

--- Guessing Game/SimpleGame.py
+++ Guessing Game/SimpleGame.py
@@ -6,22 +6,36 @@ import random
 randNum = random.randrange(0,10)
 
 #Let the user try to guess the number three time
-for guessCounter in range(3):
-    #Ask the user to guess the number
-    #Input always gives us string
+# for guessCounter in range(3):
+#     #Ask the user to guess the number
+#     #Input always gives us string
+#     userGuess = int(input('So what do you think is the number'))
+
+#     #printing the generated number and the user input
+#     print(f'The generated number is {randNum} and you guessed {userGuess}')
+
+#     #Check if he user guessed correctly
+#     if userGuess > randNum:
+#         print("Sorry, your guess is too high. Please try again")
+#     elif userGuess < randNum:
+#         print("Sorry, your guess is too low. Please try again")
+#     else:
+#         print("Congratulations. You have guessed correctly")
+#         break
+# else:
+#     print("Game over!")
+
+# Continue while the user enters the correct answer
+userGuess = -1
+while userGuess!=randNum:
+    
     userGuess = int(input('So what do you think is the number'))
 
-    #printing the generated number and the user input
-    print(f'The generated number is {randNum} and you guessed {userGuess}')
-
-    #Check if he user guessed correctly
     if userGuess > randNum:
         print("Sorry, your guess is too high. Please try again")
     elif userGuess < randNum:
         print("Sorry, your guess is too low. Please try again")
-    else:
-        print("Congratulations. You have guessed correctly")
-        break
+        
 else:
-    print("Game over!")
+    print("Congratulations you have won!")
     
