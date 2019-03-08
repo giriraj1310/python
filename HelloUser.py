1a2a1cdd7caf4d377c3d8da17f5a958de1ae62db
Xcode User <xcode@Fasts-MacBook-Pro-2.local>
2019-03-07 19:11:29 -0500

    Hello World exercises complete

---
+++ HelloUser.py
@@ -0,0 +1,8 @@
+# userName = input(f"Hello, I am {__name__}. What is your name? ")
+""" This is an interactive hello world module"""
+
+# Ask the user for their name
+userName = input("Hello, What is your name? ")
+
+#Print hello politely to the user
+print(f'Hello {userName}. Nice to meet you')
